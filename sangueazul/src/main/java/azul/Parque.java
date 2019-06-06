0902a77a812e2f94fe1058939c702c4944460b1d
sangueazul <kassianeterra@gmail.com>
2019-06-06 10:37:50 -0300

    parque gato

---
+++ sangueazul/src/main/java/azul/Parque.java
@@ -0,0 +1,13 @@
+
+package azul;
+
+
+public class Parque {
+    public static int main(String[] args){
+            Gato ad=new Gato ("azul");
+            ad.miar();
+            Gato ed=new Gato ("sangue");
+            ed.miar();
+            return 0;
+    }
+}
