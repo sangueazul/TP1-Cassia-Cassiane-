b86700e46b8493d6d41bb730503fd7ff1c2e996f
sangueazul <kassianeterra@gmail.com>
2019-07-04 10:10:02 -0300

    kanui

---
+++ sangueazul/src/main/java/azul/Cliente.java
@@ -0,0 +1,13 @@
+
+package Kanui;
+public class Cliente {   
+    String nome;
+    String sobrenome;
+    String email;
+    String sexo;
+    String cpf;
+    String nascimento;
+    
+    
+    
+}
